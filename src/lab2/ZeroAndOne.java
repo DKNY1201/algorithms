d879026ade8f77d6bf056a028514bf244a2e64c5
Quy Tran <quytran288@gmail.com>
2017-07-28 12:44:14 -0500

    Test add certification

--- src/lab2/ZeroAndOne.java
+++ src/lab2/ZeroAndOne.java
@@ -4,7 +4,7 @@ package lab2;
  * Created by Bi on 7/27/17.
  */
 public class ZeroAndOne {
-    static public void zeroAndOne(int[] arr) {
+    static public void zeroAndOne1(int[] arr) {
         int i = 0;
         int count = 0;
 
@@ -17,8 +17,61 @@ public class ZeroAndOne {
         System.out.println(arr.length - count);
     }
 
+    static public int[] zeroAndOne(int[] A) {
+        int[] zeroAndOne = new int[2];
+
+        if (A.length == 0) {
+            zeroAndOne[0] = 0;
+            zeroAndOne[1] = 0;
+            return zeroAndOne;
+        }
+
+        if (A[0] == 1) {
+            zeroAndOne[0] = 0;
+            zeroAndOne[1] = A.length;
+            return zeroAndOne;
+        }
+
+        if (A[A.length - 1] == 0) {
+            zeroAndOne[0] = A.length;
+            zeroAndOne[1] = 0;
+            return zeroAndOne;
+        }
+
+        return findBreakPoint(A, 0, A.length - 1);
+    }
+
+    static public int[] findBreakPoint(int[] A, int bot, int top) {
+        int[] zeroAndOne = new int[2];
+
+        int mid = (bot + top) / 2;
+
+        if (A[mid] == 0 && A[mid + 1] == 1) {
+            zeroAndOne[0] = mid + 1;
+            zeroAndOne[1] = A.length - (mid + 1);
+            return zeroAndOne;
+        }
+
+        if (A[mid] == 0 && A[mid + 1] != 1) {
+            return findBreakPoint(A, mid + 1, top);
+        }
+
+        if (A[mid] == 1) {
+            return findBreakPoint(A, bot, mid - 1);
+        }
+
+        return null;
+    }
+
     public static void main(String[] args) {
-        zeroAndOne(new int[] {0,0,0,0,1,1,1});
+        int[] a = zeroAndOne(new int[] {0,0,0,0,1,1,1});
+        int[] b = zeroAndOne(new int[] {0,0,0,0,0,0,0});
+        int[] c = zeroAndOne(new int[] {});
+        int[] d = zeroAndOne(new int[] {1,1,1,1,1,1,1});
+
+        for (int i: c) {
+            System.out.println(i);
+        }
     }
 }
 
